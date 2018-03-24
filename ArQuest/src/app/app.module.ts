a5dc85b22d52f208ab747f10179f7df304a3034f
Anastasiia <dark.scarlet.orhid@gmail.com>
2018-03-24 23:13:40 +0200

    register events component

--- ArQuest/src/app/app.module.ts
+++ ArQuest/src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CameraArComponent } from './camera-ar/camera-ar.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { HeaderComponent } from './camera-ar/header/header.component';
 
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'quest', component: CameraArComponent },
+  { path: 'leaderboard', component: LeaderboardComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CameraArComponent,
-    LeaderboardComponent
+    LeaderboardComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent],
