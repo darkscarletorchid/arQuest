3317a5551d382c6a70313bab18a8a5e7cbb7c245
anastasiia <you@example.com>
2018-03-24 12:03:52 +0200

    add aframe script

--- ArQuest/src/polyfills.ts
+++ ArQuest/src/polyfills.ts
@@ -1,4 +1,4 @@
-/**
+  /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
  *
@@ -17,8 +17,8 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+import 'aframe';
+  /** IE9, IE10 and IE11 requires all of the following polyfills. **/
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/object';
 // import 'core-js/es6/function';
