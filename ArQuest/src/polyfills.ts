2d4c559a7b145beda9515cc3244f9397176b9286
Anastasiia <dark.scarlet.orhid@gmail.com>
2018-03-24 20:50:58 +0200

    made it work

--- ArQuest/src/polyfills.ts
+++ ArQuest/src/polyfills.ts
@@ -17,8 +17,6 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-import 'aframe';
-import 'aframe-template-component';
   /** IE9, IE10 and IE11 requires all of the following polyfills. **/
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/object';
