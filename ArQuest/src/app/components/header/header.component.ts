792b82314d084dd6efb6bb87dc8919ea34309980
Anastasiia <dark.scarlet.orhid@gmail.com>
2018-03-27 21:22:45 +0300

    fix login layout, setup marker found event

--- ArQuest/src/app/components/header/header.component.ts
+++ ArQuest/src/app/components/header/header.component.ts
@@ -12,46 +12,13 @@ import {UserService} from '../../services/user.service';
 })
 export class HeaderComponent implements OnInit {
 
-  isAuthenticated: boolean;
-  itemsFound: Item[];
-  actualCount: number;
-  totalCount: number;
-  user: User;
-  userItem: UserItem;
-  constructor( private progressService: ProgressService,
-               private itemService: ItemService,
-               private userService: UserService) {
-    // TODO remove stub
-    this.user = {
-      id: 1,
-      username: 'Nastia'
-    };
-    this.totalCount = 10;
-    this.actualCount = 0;
-    this.isAuthenticated = true;
+
+  constructor() {
   }
 
   ngOnInit() {
-    // this.user = this.userService.getCurrentUser();
-    // this.getProgressByUser(this.user.id);
-    // this.actualCount = this.itemsFound.length;
-    // this.itemService.getAll().subscribe(items => this.totalCount = items.length, error => {});
+
   }
 
-  getProgressByUser(id: number): void {
-    this.progressService.getProgressByUser(id)
-      .subscribe(items => this.itemsFound = items, error => {});
-  }
 
-  @HostListener('markerFound', ['$event'])
-  onMarkerFound(e) {
-    this.userItem.itemId = e.detail.markerId;
-    this.userItem.userId = this.user.id;
-    this.progressService.addToProgress(this.userItem)
-      .subscribe(result => this.progressService.getProgressByUser(this.user.id)
-        .subscribe(items => {
-          this.itemsFound = items;
-          this.actualCount = items.length;
-        }));
-  }
 }
