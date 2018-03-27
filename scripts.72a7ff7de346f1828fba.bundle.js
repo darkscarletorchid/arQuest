21fe11090f9498020781e2028930add452072668
Anastasiia <dark.scarlet.orhid@gmail.com>
2018-03-27 22:14:53 +0300

    Auto-generated commit

---
+++ scripts.72a7ff7de346f1828fba.bundle.js
@@ -0,0 +1,1 @@
+AFRAME.registerComponent("registerevents",{init:function(){var e=this.el;e.addEventListener("markerFound",function(n){var t=e.id;window.dispatchEvent(new CustomEvent("markerFound")),alert("markerFound "+t)}),e.addEventListener("markerLost",function(){})}});
--- scripts.91b3df8db6fca6093459.bundle.js
+++
@@ -1,1 +0,0 @@
-AFRAME.registerComponent("registerevents",{init:function(){var e=this.el;e.addEventListener("markerFound",function(n){var t=e.id;window.dispatchEvent(new CustomEvent("markerFound",{detail:{markerId:t}})),alert("markerFound "+t)}),e.addEventListener("markerLost",function(){})}});
